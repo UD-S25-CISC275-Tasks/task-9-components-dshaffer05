f9a7737d15d9ee10bfeea9f009302b3fd7472855
dshaffer <dshaffer@udel.edu>
2025-03-07 16:53:50 -0500

    Completed changetype, counter, and revealanswer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [reveal, revealAnswer] = useState<boolean>(false);
+
+    function revealIt(): void {
+        revealAnswer(!reveal);
+    }
+
+    return (
+        <div>
+            <Button onClick={revealIt}>Reveal Answer</Button>
+            {reveal && <div>The answer is 42.</div>}
+        </div>
+    );
 }
