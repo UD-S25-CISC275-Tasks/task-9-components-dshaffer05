d5c28d020ebd5203f635af4e51fce20724d186bb
dshaffer <dshaffer@udel.edu>
2025-03-11 13:16:23 -0400

    ChooseTeam and DoubleHalf Complete

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): React.JSX.Element {
+/*function Doubler(): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -12,9 +11,9 @@ function Doubler(): React.JSX.Element {
             Double
         </Button>
     );
-}
+}*/
 
-function Halver(): React.JSX.Element {
+/*function Halver(): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -24,17 +23,30 @@ function Halver(): React.JSX.Element {
             Halve
         </Button>
     );
-}
+}*/
 
 export function DoubleHalf(): React.JSX.Element {
+    const [dhValue, setDhValue] = useState(10);
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Button
+                onClick={() => {
+                    setDhValue(2 * dhValue);
+                }}
+            >
+                Double
+            </Button>
+            <Button
+                onClick={() => {
+                    setDhValue(0.5 * dhValue);
+                }}
+            >
+                Halve
+            </Button>
         </div>
     );
 }
